92c06ccb5e8b38bae4646976de884b9a8e598895
Adam Dodge <815495@mystma.org>
2021-01-15 11:45:23 -0600

    Added Moving Platform
    Added Moving Platform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,13 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rb;
 
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>(); 
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,6 +31,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+            sr.flipX = false;
         }
 
         if (Input.GetKeyUp(KeyCode.RightArrow))
@@ -38,11 +42,28 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            sr.flipX = true;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D c)
+    {
+        if (c.gameObject.tag == "Platform")
+        {
+            transform.SetParent(c.transform);
+        }
+    }
+
+    private void OnCollisionExit2D (Collision2D c)
+    {
+        if (c.gameObject.tag == "Platform")
+        {
+            transform.SetParent(null);
         }
     }
 }
